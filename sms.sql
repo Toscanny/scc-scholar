5876a1b04dcf80b52470120faeb5cf12b308b334
criscdama <criscia@1nurse.com>
2022-11-23 01:01:36 +0800

    UPDATED SMS SQL

--- sms.sql
+++ sms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2022 at 04:49 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.8
+-- Generation Time: Nov 22, 2022 at 05:59 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -46,7 +46,7 @@ CREATE TABLE `tbl_admin` (
 --
 
 INSERT INTO `tbl_admin` (`admin_id`, `admin_idno`, `admin_email_address`, `admin_password`, `admin_name`, `admin_position`, `admin_address`, `admin_contact_no`, `admin_verification_code`, `admin_user_otp`, `admin_email_verify`) VALUES
-(1, 'SCC-0001368', 'unswaa20@gmail.com', '$argon2i$v=19$m=65536,t=4,p=1$TDZHeTg0NXp5S2JUVFZNSg$36gjUAd6KxFAcsDzinlxtKthHoOkoJesox3WUuPEUs8', 'Administrator', 'Chairman', 'Natalio B. Bacalso S National Hwy, Minglanilla, Cebu', '(032) 490 8511', '269010', '', 'Yes');
+(1, 'SCC-0001368', 'tjiecarmen90@gmail.com', '$argon2i$v=19$m=65536,t=4,p=1$TDZHeTg0NXp5S2JUVFZNSg$36gjUAd6KxFAcsDzinlxtKthHoOkoJesox3WUuPEUs8', 'Administrator', 'Chairman', 'Natalio B. Bacalso S National Hwy, Minglanilla, Cebu', '(032) 490 8511', '961571', '', 'No');
 
 -- --------------------------------------------------------
 
@@ -80,15 +80,33 @@ CREATE TABLE `tbl_newstudent` (
   `s4psno` varchar(20) DEFAULT NULL,
   `spcyincome` varchar(20) DEFAULT NULL,
   `S_disability` varchar(20) DEFAULT NULL,
-  `s_datefil` varchar(20) DEFAULT NULL
+  `s_datefil` varchar(20) DEFAULT NULL,
+  `s_scholar_stat` varchar(20) NOT NULL,
+  `s_scholarship_type` varchar(20) NOT NULL,
+  `snext` varchar(10) NOT NULL,
+  `spscourse` varchar(20) NOT NULL,
+  `scsyrlvl` varchar(20) NOT NULL,
+  `spwdid` varchar(20) NOT NULL,
+  `ssdfile` varchar(10) NOT NULL,
+  `sdstbytpic` varchar(20) NOT NULL,
+  `sdstbytpicstat` varchar(20) NOT NULL,
+  `sdspsa` varchar(20) NOT NULL,
+  `sdspsastat` varchar(20) NOT NULL,
+  `sdsbrgyin` varchar(20) NOT NULL,
+  `sdsbrgyinstat` varchar(20) NOT NULL,
+  `spass` varchar(100) NOT NULL,
+  `s_verification_code` varchar(100) NOT NULL,
+  `s_email_verify` varchar(10) NOT NULL,
+  `s_account_status` varchar(20) NOT NULL,
+  `s_scholarship_note` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_newstudent`
 --
 
-INSERT INTO `tbl_newstudent` (`s_id`, `ss_id`, `s_image`, `slname`, `sfname`, `smname`, `sgender`, `sdbirth`, `scontact`, `saddress`, `spschname`, `sccourse`, `syrlvl`, `semail`, `sflname`, `sffname`, `sfmname`, `sfoccu`, `smlname`, `smfname`, `smmname`, `smoccu`, `s4psno`, `spcyincome`, `S_disability`, `s_datefil`) VALUES
-(1, 'Scc-048294234', NULL, 'Paradero', 'Lira Mae', 'Tapasao', 'Female', '05-17-2000', '566243424', 'rsfsdsv', 'gfgfgsg', 'gsgs', 'fsdfsdf', 'sdfsf', 'fsdf', 'fsfs', 'fsdf', 'ghfgh', 'adas', 'vvxc', 'sadsad', 'ffa', 'fafadf', 'fdfs', 'fsdfs', 'fsdfsdf');
+INSERT INTO `tbl_newstudent` (`s_id`, `ss_id`, `s_image`, `slname`, `sfname`, `smname`, `sgender`, `sdbirth`, `scontact`, `saddress`, `spschname`, `sccourse`, `syrlvl`, `semail`, `sflname`, `sffname`, `sfmname`, `sfoccu`, `smlname`, `smfname`, `smmname`, `smoccu`, `s4psno`, `spcyincome`, `S_disability`, `s_datefil`, `s_scholar_stat`, `s_scholarship_type`, `snext`, `spscourse`, `scsyrlvl`, `spwdid`, `ssdfile`, `sdstbytpic`, `sdstbytpicstat`, `sdspsa`, `sdspsastat`, `sdsbrgyin`, `sdsbrgyinstat`, `spass`, `s_verification_code`, `s_email_verify`, `s_account_status`, `s_scholarship_note`) VALUES
+(11, '234789', '', 'Carmen', 'Toscanny', 'Sellote', 'male', '2022-11-22', '09664932334', 'Cadulawan, Minglanilla', 'St. Cecilia College', 'BSIT', '4', 'tjiecarmen90@gmail.com', 'Serate', 'Carmen', 'Glenn', 'Driver', 'Sellote', 'Carmen', 'Leone', 'Dressmaker', '6', NULL, '653678', '', 'Approved', 'UNIFAST', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -202,12 +220,14 @@ CREATE TABLE `tbl_student` (
 
 INSERT INTO `tbl_student` (`s_id`, `ss_id`, `sfname`, `smname`, `slname`, `snext`, `sdbirth`, `sgender`, `saddress`, `szcode`, `scontact`, `semail`, `sctship`, `scivilstat`, `spbirth`, `sdisability`, `s4psno`, `spwdid`, `srappsship`, `srappnas`, `sbos`, `ssskills`, `stwinterest`, `ssdfile`, `sgfname`, `sgmname`, `sglname`, `sgnext`, `sglstatus`, `sgeduc`, `sgcontact`, `sgaddress`, `sgoccu`, `sgcompany`, `sffname`, `sfmname`, `sflname`, `sfnext`, `sflstatus`, `sfeduc`, `sfcontact`, `sfaddress`, `sfoccu`, `sfcompany`, `smfname`, `smmname`, `smlname`, `smnext`, `smlstatus`, `smeduc`, `smcontact`, `smaddress`, `smoccu`, `smcompany`, `snsibling`, `spcyincome`, `spschname`, `spsaddress`, `spstype`, `spscourse`, `spsyrlvl`, `spsgwa`, `spsraward`, `spsdawardrceive`, `scsintend`, `scsadd`, `scschooltype`, `sccourse`, `sccourseprio`, `scsyrlvl`, `spass`, `sdsprc`, `sdsprcstat`, `sdspgm`, `sdspgmstat`, `sdspcr`, `sdspcrstat`, `sdstbytpic`, `sdstbytpicstat`, `sdsbrgyin`, `sdsbrgyinstat`, `sdscef`, `sdscefstat`, `sdspsa`, `sdspsastat`, `sdsobr`, `sdsobrstat`, `s_scholarship_note`, `s_added_on`, `s_applied_on`, `s_verification_code`, `s_user_otp`, `s_email_verify`, `s_account_status`, `s_grant_stat`, `s_scholar_stat`, `s_scholarship_type`) VALUES
 (982, 'SCC-18-0007077', 'Vincent Xyron', 'A.', 'Albina', 'N/A', '1999-02-07', 'M', '651 South Boundary Tungkop, Minglanilla, Cebu', NULL, '0933-933-0521', NULL, NULL, 'Single', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Mary Ann A. Albina', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Arman Albina', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Instructor', NULL, 'Mary Ann A. Albina', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Housewife', NULL, NULL, NULL, 'scc', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'BSIT', NULL, '3rd Year', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, 'Active', '', '', 'New Unifast'),
-(984, 'SCC-18-0005842', 'Jhezer', 'R.', 'Libor', 'N/A', '1998-11-01', 'Male', 'Langtad, City of Naga, Cebu', NULL, '9124345667', 'jhezerlibor@yopmail.com', 'Filipino', 'Single', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Alma R. Libor', '', '', '', NULL, NULL, 'Ea suscipit cillum a', 'Officia qui voluptat', 'Ipsa atque id eos', 'Charles and Sexton Trading', 'Jeffrey Y. Libor', '', '', '', NULL, NULL, 'Est excepteur volup', 'Eveniet magnam volu', 'Lorem sint in qui ev', 'Dickson and Howell Plc', 'Alma R. Libor', '', '', '', NULL, NULL, 'Eaque non voluptates', 'Rem ut id quisquam q', 'Lorem suscipit venia', 'Delgado Wyatt Co', NULL, '995', 'scc', NULL, NULL, NULL, NULL, 88, 'N/A', '2022-02-17', NULL, NULL, NULL, 'BSIT', NULL, '3rd Year', '$argon2i$v=19$m=65536,t=4,p=1$Y2dYcDlJYW5DdTlUNGRzQw$N8aHQyRlGtyPMJ8OddIercW+WFXNG/0m8nGPhfBwJvc', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-02-18', '2022-02-18', 'dff8e49d5e315305002935b8de9ac9b5', '', 'Yes', 'Active', '', 'Approved', 'Academic'),
-(986, 'SCC-16-0003180', 'Angelika', 'N/A', 'Singcol', 'N/A', '1999-10-05', 'Female', 'Pungsod Lawa-an 3, Talisay City', '6045', '9673159901', 'angelikasingcol945@gmail.com', 'Filipino', 'Single', NULL, 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', NULL, 'Ricky Sarsalijo', 'Quintano', 'Sarsalijo', 'N/A', 'Living', 'High school graduate', '09560561438', 'Lawaan 3 Talisay city', 'Helper', 'Singcol sticker &amp;amp;amp;a', 'Ricky Sarsalijo', 'Quintano', 'Sarsalijo', 'N/A', 'Living', 'High school graduate', '09560561438', 'Lawaan 3 Talisay city', 'construction', 'Singcol sticker &amp;amp;amp;a', 'Jessica Singcol', 'Bueno', 'Singcol', 'N/A', 'Living', 'High school graduate', '09560561438', 'Lawaan 3 Talisay city', 'Housewife', 'N/A', '2', '12,00', 'scc', 'Poblacion ward 2 minglanilla cebu', 'Private', 'Bsit', '4th yr', NULL, NULL, NULL, 'St.cecilia\'s college cebu-inc', 'Poblacion ward 2', 'Private', 'BSIT', 'Priority', '4th Year', '$argon2i$v=19$m=65536,t=4,p=1$VmdQR0lQVkIuVkxEL3VROA$mIstmuuDnN74uk8flQodpNQHgFThUJWV1+kH4bpJCE4', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-02-18', '2022-02-18', 'bba6de0e7aac6af923bab2574aabaf19', '', 'Yes', 'Active', '', 'Approved', 'Non-Academic'),
 (988, 'SCC-18-0008647', 'Junmar', 'T.', 'Cavalida', NULL, '1999-11-03', 'M', 'Aloguinsan Cebu', NULL, '0991-742-7817', 'cilinjohnrey@gmail.com', NULL, 'Single', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Virginia Cavalida', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Mariano Cavalida', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Driver', NULL, 'Virginia Cavalida', NULL, NULL, NULL, NULL, NULL, NULL, NULL, ' ', NULL, NULL, NULL, 'SCC', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'BSIT', NULL, '3rd Year', '$argon2i$v=19$m=65536,t=4,p=1$VlZRemJqQU9udmRsWDhEZw$Vd38pJJGBDvIBl3on3YktFF0O3zR8Nl8xZzTjaF4fJc', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-02-19', NULL, '3802688be78812b618ed4265075492de', '', 'Yes', 'Active', '', '', 'New Unifast'),
-(989, 'SCC-16-0004091', 'John Carlo', 'G.', 'Largo', 'N/A', '2000-03-30', 'Male', 'Poblacion Ward 2 Minglanilla Cebu', '6046', '09685859243', 'carlolargo@yopmail.com', 'Filipino', 'Single', 'Minglanill', 'N/A', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Vilma G. Largo', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Santos F. Largo', NULL, NULL, NULL, 'Living', 'High School', NULL, 'Poblacion Ward 2 Minglanilla, Cebu', 'Utility Worker', NULL, 'Vilma G. Largo', NULL, NULL, NULL, 'Living', 'High School', NULL, 'Poblacion Ward 2 Minglanilla, Cebu', 'Manicurist', NULL, '2', '10000', 'scc', 'Ward 2', 'Private', NULL, 'Grade 12', NULL, NULL, NULL, 'St. Cecilia\'s College', 'Ward 2', 'Private', 'BSIT', 'Priority', '3rd Year', '$argon2i$v=19$m=65536,t=4,p=1$QWwwUTRjb083SmRSTndQaA$+jVxm2RkMj2ONlO/riWxrBw3r/cNPJQc84ZQ6RHO91Y', '', 'Not-Received', '', 'Not-Received', NULL, NULL, NULL, NULL, '', 'Not-Received', NULL, NULL, NULL, NULL, NULL, NULL, 'N/A', '2022-02-18', '2022-02-18', 'bc1159410407e3fd08503c209f2f7d24', '', 'Yes', 'Active', '', 'Renewal', 'CHED'),
 (990, 'SCC-18-0007536', 'Jun Carlo', '', 'Oros', NULL, '1997-09-12', 'M', 'Mayana City of Naga', NULL, '0961-604-1438', 'juncarlooros@yopmail.com', NULL, 'Single', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Eladia Oros', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Clito Oros', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Construction worker', NULL, 'Eladia Oros', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, NULL, 'scc', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'BSIT', NULL, '3rd Year', '$argon2i$v=19$m=65536,t=4,p=1$QjRzOS9wMXNMUkVHLjRoOA$uo5pPHBCwFajGh8NQ+I0s2Mq5TAXU5+fZhMiq1wYL7I', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-02-19', NULL, 'aef2faa404bd8705d67df54ef8bc8707', '', 'No', 'Active', '', '', 'New Unifast'),
-(991, 'SS-17-0004665', 'Lira Mae', 'Tapasao', 'Paradero', 'BSIT 4th', '05/17/2000', 'Female', 'Tungkop', '6046', '09321315494', 'paraderolira17@gmail.com', 'Filipino', 'Single', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, 'Inactive', '', '', '');
+(991, 'SS-17-0004665', 'Lira Mae', 'Tapasao', 'Paradero', 'BSIT 4th', '05/17/2000', 'Female', 'Tungkop', '6046', '09321315494', 'paraderolira17@gmail.com', 'Filipino', 'Single', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, 'Inactive', '', '', ''),
+(992, '2131321', 'Criscia', 'Carmen', 'Dama', 'N/A', '2012-02-09', 'Female', 'tagum', NULL, '09155813389', 'criscia@1nurse.com', NULL, NULL, NULL, NULL, 'na', 'na', NULL, NULL, NULL, NULL, NULL, '2022-11-21', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Ferdinand', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Jean', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'test', 'na', NULL, NULL, 'na', 'na', NULL, NULL, NULL, NULL, NULL, NULL, 'BSIT', NULL, '4th Year', '$argon2i$v=19$m=65536,t=4,p=1$ZzI0MHJ5d1lRTkpoWHVZQg$1gs4q6vL7QtoOon6eSZfmLVB6sINOUoQ5dOjFOqsinM', NULL, NULL, NULL, NULL, NULL, NULL, '2022-11-18', 'Received', '2022-11-21', 'Received', NULL, NULL, '2022-11-19', 'Received', NULL, NULL, 'na', NULL, '2022-11-21', '41b4002147f103094230a2e37aa28da3', '', 'No', 'Active', '', 'Rejected', 'UNIFAST'),
+(993, '2131321', 'Toscanny', 'NA', 'Carmen', 'N/A', '2022-11-10', 'Male', 'NA', NULL, '096546534', 'toscanny@gmail.com', NULL, NULL, NULL, NULL, 'NA', 'NA', NULL, NULL, NULL, NULL, NULL, '2022-10-31', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'na', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'na', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'NA', 'NA', NULL, NULL, 'test', 'NA', NULL, NULL, NULL, NULL, NULL, NULL, 'BSIT', NULL, '4th Year', '$argon2i$v=19$m=65536,t=4,p=1$WmgwWG4yazF2R1JDT1diOQ$Xx7tXXj6z2rHLIHBi4LyHwYqt2Y0ha/nzB7pQnBTHIk', NULL, NULL, NULL, NULL, NULL, NULL, '2022-11-15', 'Received', '2022-11-22', 'Received', NULL, NULL, '2022-11-23', 'Received', NULL, NULL, 'NA', NULL, '2022-11-21', 'f2a674a861de5c6e57909b9bb7dd7ae7', '', 'No', 'Active', '', 'Rejected', 'UNIFAST'),
+(994, '2131321', 'Criscia', 'NA', 'Dama', 'N/A', '2022-11-17', 'Female', 'NA', NULL, '09155813389', 'damacriscia@gmail.com', NULL, NULL, NULL, NULL, 'NA', 'NA', NULL, NULL, NULL, NULL, NULL, '2022-11-21', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Ferdinand', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'na', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'NA', 'NA', NULL, NULL, 'BSIT', '4th Year', NULL, NULL, NULL, NULL, NULL, NULL, 'BSIT', NULL, '4th Year', '$argon2i$v=19$m=65536,t=4,p=1$REwuenhkNXkwcWdnc0oxZw$QLrdoBkjxJW02RBAUANwkLGh3UeQbWVH0UJ1KcRnS84', NULL, NULL, NULL, NULL, NULL, NULL, '2022-11-20', 'Received', '2022-11-22', 'Received', NULL, NULL, '2022-11-22', 'Received', NULL, NULL, 'NA', NULL, '2022-11-21', '6ea0b386c810290aca67bad9d55fb58d', '', 'No', 'Active', '', 'Approved', 'UNIFAST'),
+(995, '000000', 'Dimple', 'Carmen', 'Carmen', 'N/A', '2022-11-17', 'Female', 'NA', NULL, '09564645634', 'dimple@gmail.com', NULL, NULL, NULL, NULL, 'NA', 'NA', NULL, NULL, NULL, NULL, NULL, '2022-11-16', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'na', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'na', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'NA', 'NA', NULL, NULL, 'NA', '3rd Year', NULL, NULL, NULL, NULL, NULL, NULL, 'BSHM', NULL, '3rd Year', '$argon2i$v=19$m=65536,t=4,p=1$Z0dZREkuUEd2NTAzVDQ3dQ$AiGwIlwWorlhR/w0UbUiqIxv4ELtEtRq1XtBkWwrosk', NULL, NULL, NULL, NULL, NULL, NULL, '2022-11-16', 'Received', '2022-11-15', 'Received', NULL, NULL, '2022-11-15', 'Received', NULL, NULL, 'dsad', NULL, '2022-11-21', '6b3df67dbcb8cd90c8ece294c64c4896', '', 'No', 'Active', '', 'Approved', 'UNIFAST'),
+(996, '2131321', 'Dimple', 'NA', 'Carmen', 'N/A', '2022-11-09', 'Female', 'tgghj', NULL, '09155813389', 'dimpley@gmail.com', NULL, NULL, NULL, NULL, 'test', 'test', NULL, NULL, NULL, NULL, NULL, '2022-11-18', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Ferdinand', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Jean', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'test', 'frhh', NULL, NULL, 'ytyh', '87', NULL, NULL, NULL, NULL, NULL, NULL, 'BSHM', NULL, '4th Year', '$argon2i$v=19$m=65536,t=4,p=1$Y2R5M2dBT2htTGU5Y29Jdg$EgkOFjI/wVngc8Y9A/ezGHvhjGh62U5ucT1q4hsj19o', NULL, NULL, NULL, NULL, NULL, NULL, '2022-11-16', 'Received', '2022-11-24', 'Received', NULL, NULL, '2022-11-11', 'Received', NULL, NULL, 'hhu', NULL, '2022-11-21', '605a18ef3772273733fb1983c9efe28d', '', 'No', 'Active', '', 'Approved', 'UNIFAST');
 
 -- --------------------------------------------------------
 
@@ -236,7 +256,9 @@ INSERT INTO `users` (`S_id`, `username`, `password`, `created_at`) VALUES
 (7, 'sumalinao', '$2y$10$WCbd99MQEezMaQBa1Et8uu7j8okLlUzhMcDnLHyiWrw8FqZHTMGkO', '2022-09-30 15:38:23'),
 (8, 'ARRARRARRA', '$2y$10$1K1wHNCERTBJDot5tkyIQeX/wAQsvJ9bdu6TW.PEQQ9YxjDBcScE6', '2022-09-30 22:35:02'),
 (9, 'liramae', '$2y$10$xcy6QSGs0x5n8TO3cF5HO.ciZd9CYLQM92VFly0dXd6f38iU9lCQ6', '2022-10-17 21:57:12'),
-(10, 'liraliraliralira', '$2y$10$3.5em.tWuWKkQ3tpXQnZgO3i2yMo38sbWrEzppOvcqLGaDfEVklSm', '2022-10-18 23:37:47');
+(10, 'liraliraliralira', '$2y$10$3.5em.tWuWKkQ3tpXQnZgO3i2yMo38sbWrEzppOvcqLGaDfEVklSm', '2022-10-18 23:37:47'),
+(11, 'criscia', '$2y$10$JSpVE1MLG09quj0GlKxA/OLMQp.j51jMZ2dLO1jgk6IftZv9ZWEm6', '2022-11-21 03:01:27'),
+(12, 'tricia_shang', '$2y$10$gKhDrgy7MjHbqdwFqQqbSuO5yi3UBVcYUXv8gl1FjRtutnH.ymB6e', '2022-11-21 17:54:34');
 
 --
 -- Indexes for dumped tables
@@ -281,19 +303,19 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_newstudent`
 --
 ALTER TABLE `tbl_newstudent`
-  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `tbl_student`
 --
 ALTER TABLE `tbl_student`
-  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=992;
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=997;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `S_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `S_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
